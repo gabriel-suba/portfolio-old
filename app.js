c140c27905d75343798bf077516d525e4516dba4
gab <wr3gab@gmail.com>
2022-11-24 14:07:28 +0800

    added animation on about section

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const projects = document.querySelectorAll('.project')
+const section_header = document.querySelector('.section-header')
+const about_title = document.querySelectorAll('.about__title')
+const about_paragraph = document.querySelectorAll('.about__paragraph')
+const about_name = document.querySelector('.about__name')
 
 const options = {
-	// threshold: 0,
-	rootMargin: '-300px'
+	threshold: 0.3,
+	// rootMargin: '-300px'
 }
 
 const observer = new IntersectionObserver(entries => {
@@ -15,3 +19,15 @@ const observer = new IntersectionObserver(entries => {
 projects.forEach(project => {
 	observer.observe(project)
 })
+
+about_title.forEach(title => {
+	observer.observe(title)
+})
+
+about_paragraph.forEach(paragraph => {
+	observer.observe(paragraph)
+})
+
+observer.observe(section_header)
+
+observer.observe(about_name)
